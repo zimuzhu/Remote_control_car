889e0884a308909a78112841337150b931e30d4c
梓木竹 <134277771+zimuzhu@users.noreply.github.com>
2023-09-24 15:21:52 +0800

    Delete hardware directory

--- hardware/PWM1.h
+++
@@ -1,6 +0,0 @@
-//#ifndef  __PWM1_H
-//#define	__PWM1_H
-//void PWM_Init1(void);//Æô¶¯¶¨Ê±
-//void B0(uint16_t Compare);
-
-//#endif
--- hardware/SHEXIANGTOU.h
+++
@@ -1,9 +0,0 @@
-//#ifndef __SHEXIANGTOU_H
-//#define __SHEXIANGTOU_H
-
-//int Angle_Init(void);
-//void B0Angle(float Angle);
-//void lift9(void);
-//void put9(void);
-
-//#endef 
